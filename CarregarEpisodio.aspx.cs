5334ccff70942846be3a40abddf3b6971d1ee95e
amosbatista <amos.silva@gmail.com>
2015-10-12 21:26:14 -0300

    Eu mesclei as pastas principais com o WebServer, já que eles estarão juntos, de qualquer forma. Com isto, o front da página e do server de JSON estão juntos.
    - O banco está pronto;
    - O acesso ao banco e o cadastro do sistema estão prontos.
    - A visualização está funcional.
    
    Signed-off-by: amosbatista <amos.silva@gmail.com>

---
+++ CarregarEpisodio.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using AmosBatista.ComicsServer.Core;
+
+// URL Example: http://localhost:3472/CarregarEpisodio.aspx?episodeNumber=1&idiom=en
+namespace AmosBatista.ComicServer.WebServer
+{
+    public partial class CarregarEpisodio : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Expires = -1;
+            if (Request["episodeNumber"] != null)
+            {
+                ParametrosPesquisaEpisodio parametros = new ParametrosPesquisaEpisodio();
+                parametros.NovoParametro("NUMEROEPISODIO", Request["episodeNumber"]);
+                parametros.NovoParametro("IDIOMA", Request["idiom"]);
+                IEpisode NovoEpisode = new Episode();
+                IEpisodeRepository epsRep = new EpisodeRepositoryToSQLServer();
+                NovoEpisode = epsRep.Buscar(parametros);
+
+                Response.Clear();
+                Response.ContentType = "application/json; charset=utf-8";
+                Response.Write(NovoEpisode.EpisodeContent.ToJSONString());
+                Response.End();
+            }
+            else
+            {
+                Response.Write("Page is loaded. Please, execute using the comics Page.");
+            }
+        }
+    }
+}
---
+++ SalvarEpisodio.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using AmosBatista.ComicsServer.Core;
+
+// URL Example: http://localhost:3472/SalvarEpisodio.aspx?episodeNumber=1&idiom=en&jsonContent=JsonTest
+
+namespace AmosBatista.ComicServer.WebServer
+{
+    public partial class SalvarEpisodio : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.Form["episodeNumber"] != null)
+            {
+                //int episodeNumber, string idiom, string jsonContent
+                string episodeNumber = Request.Form["episodeNumber"];
+                string idiom = Request.Form["idiom"];
+                string jsonContent = Request.Form["jsonContent"];
+
+                IEpisode episode = new Episode();
+                BasicStyleEpisodeContent episodeContent = new BasicStyleEpisodeContent();
+                episodeContent.JSONEpisode = jsonContent;
+                episode.EpisodeContent = episodeContent;
+                episode.EpisodeNumber = Int32.Parse( episodeNumber);
+                episode.Idiom = idiom;
+                IEpisodeRepository epsRep = new EpisodeRepositoryToSQLServer();
+                epsRep.Salvar(episode);
+                Response.Write("Episodio Salvo");
+                Response.End();
+            }
+            else
+            {
+                Response.Write("Page is loaded. Please, execute using the comics Page.");
+            }
+        }
+    }
+}

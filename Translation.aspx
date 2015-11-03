870dae36da3075aef6c626a9a06ae38477343b56
amosbatista <amos.silva@gmail.com>
2015-11-02 23:17:21 -0200

    - Inclusion of translation repository. - Definition of "pages" (internal DIVs inside the main page). - Creation of About page.
    Signed-off-by: amosbatista <amos.silva@gmail.com>

---
+++ Translation.aspx
@@ -0,0 +1,2 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Translation.aspx.cs" Inherits="AmosBatista.ComicServer.WebServer.Translation" %>
+
---
+++ Translation.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using AmosBatista.ComicsServer.Core.StringRepository;
+
+// URL Example: http://localhost:3472/Translation.aspx
+
+namespace AmosBatista.ComicServer.WebServer
+{
+    public partial class Translation : System.Web.UI.Page
+    {
+        // This page will work to return all the caption of the text to the page, by JSON
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var textRepository = new TextRepository_SQLToJSON(false);
+
+            // Set the response time to JSON
+            Response.Clear();
+            Response.ContentType = "application/json;";
+
+            // Sending all translation to the page
+            Response.Write(textRepository.LoadTextRepository());
+            Response.End();
+
+        }
+    }
+}
